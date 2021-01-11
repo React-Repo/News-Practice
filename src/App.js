dc49db73e615548c52858bf64c666a8a7f607152
sunyeongchoi <sn0716@naver.com>
2021-01-12 02:00:59 +0900

    외부api연동하여 뉴스 뷰어 만들기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React from "react";
+import {Route} from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try{
-      const response = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=fc444b3a393c49eb99efd6f63d669444',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e)
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+    return <Route path='/:category?' component={NewsPage} />;
 };
 
 export default App
